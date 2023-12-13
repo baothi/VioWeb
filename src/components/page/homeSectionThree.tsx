861f81c0243b39d7f953dc1eb26a9c02f1112ff5
thinguyen3131 <thi.nguyen@prismtechinc.io>
2023-12-13 14:03:34 +0700

    fix layout

--- src/components/page/homeSectionThree.tsx
+++ src/components/page/homeSectionThree.tsx
@@ -25,7 +25,7 @@ const HomeSectionThree = () => {
     return (
       <section className="section home_sec_3">
         <div className="grid-container">
-            <div className="grid-50" data-aos="fade-right">
+            <div className="grid-50" data-aos="fade-right" data-aos-duration="3000">
                 <ul className="tab_img">
                   {tabItems.map(item => (
                     <li key={item.id} className={activeTab === item.id ? 'active' : ''}>
@@ -35,11 +35,11 @@ const HomeSectionThree = () => {
                 </ul>
             </div>
             <div className="grid-50 tablet-grid-50">
-                <h2 className="heading_title" data-aos="fade-left">
+                <h2 className="heading_title" data-aos="fade-left" data-aos-duration="3000">
                     <b>Vio</b> simplifies<br/>
                     your <span>Life</span>
                 </h2>
-                <ul className="tab_content" data-aos="fade-left">
+                <ul className="tab_content" data-aos="fade-left" data-aos-duration="3000">
                 {tabItems.map(item => (
                   <li
                     key={item.id}
